beef9566f5a853f5d62ce1e39e4eba11fd0260d7
hoangtranson <son.hoang01@gmail.com>
2023-01-20 19:59:47 +0700

    add get books and get single book api

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-  title = models.CharField(max_length=255)
-  author = models.CharField(max_length=255)
-  price = models.DecimalField(max_digits=5, decimal_places=2)
 
-  class Meta:
-      indexes = [
-          models.Index(fields=['price']),
-      ]
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books),
+    path('books', views.BookView.as_view()),
+    path('books/<int:pk>', views.SingleBookView.as_view()),
 ]
